584d2066d2ed08ce8ab253fd45b302476f202759
Mark John Saspa <markjonhsaspa@gmail.com>
2022-12-12 15:00:10 +0800

    php course

---
+++ 02_variables.php
@@ -0,0 +1,33 @@
+<?php
+// $name = 'Mark'; //string
+// $age = 40; //number
+// $has_kids = true;
+
+// echo $has_kids; // if true it will return 1 if false it will return nothing
+// var_dump($has_kids) // it will return 1bol(true)
+
+// echo 'my name is ' . $name . ' and i am ' . $age . ' years old' // if you use single quotes here
+
+//using double quotes
+
+// echo "my name is $name and i am $age years old"
+
+// we can also use the same thing as javascript
+
+// echo "my name is ${name} and i am ${age} years old"
+
+// echo 55+5
+// echo '5' + '5'
+// echo "5" +  "5" //i don't know why it is not considered as string
+// $x = 5 + 5
+// var_dump($x)
+// echo 10 - 5
+// echo 5*6
+// echo 10/2
+// echo 10%3
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo DB_NAME;
+?>
