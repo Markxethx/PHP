4ed3448c9b59ba28be9aacb6bda43046105f4315
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2024-01-05 23:28:27 +0800

    Create multiDimensionalArray.php

---
+++ multiDimensionalArray.php
@@ -0,0 +1,22 @@
+<?php 
+	//multi dimensional array
+ $blogs = [
+ ['mario party', 'mario', 'lorem', 30],
+ ['mario cart cheats', 'toad', 'lorem', 25],
+ ['zelda hidden chests', 'link', 'lorem', 50]
+ ]
+
+print_r($blogs);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Php tuts
+</title>
+</head>
+
+<body>
+</body>
+</html>
